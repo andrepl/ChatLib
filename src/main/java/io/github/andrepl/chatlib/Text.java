fcbd0147509faa2c677e75a733a8b479cb8a2b62
DoctorDark <DoctorDark@users.noreply.github.com>
2014-07-02 21:52:44 +0100

    Added option to set the colour of appended stack

--- src/main/java/io/github/andrepl/chatlib/Text.java
+++ src/main/java/io/github/andrepl/chatlib/Text.java
@@ -39,6 +39,13 @@ public class Text extends ChatComponentText {
 		return append(new Text(nms.getName()).setColor(ChatColor.getByChar(nms.w().e.getChar())).setHover(HoverAction.SHOW_ITEM, new ChatComponentText(tag.toString())));
 	}
 
+	public Text appendItem(ItemStack stack, ChatColor color) {
+		net.minecraft.server.v1_7_R3.ItemStack nms = CraftItemStack.asNMSCopy(stack);
+		NBTTagCompound tag = new NBTTagCompound();
+		nms.save(tag);
+		return append(new Text(nms.getName()).setColor(color).setHover(HoverAction.SHOW_ITEM, new ChatComponentText(tag.toString())));
+	}
+
 	public Text setBold(boolean bold) {
 		getChatModifier().setBold(bold);
 		return this;
